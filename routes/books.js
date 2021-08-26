d599a5f067643bc312d4bcb7233fe983098f5a71
gironalex <giron.alex@outlook.com>
2021-08-26 14:12:42 -0400

    fixed creating a book entry

--- routes/books.js
+++ routes/books.js
@@ -16,13 +16,61 @@ function asyncHandler(cb){
 
 /* GET books listing. */
 router.get('/', asyncHandler(async (req, res) => {
-  const books = await Book.findAll( { order: [['title', 'ASC']]} );
+  const books = await Book.findAll();
   res.render('index', { books: books, title: 'Books' }); 
 }));
 
-/* Creating new Book form */
+/* GET new Book form */
 router.get('/new', asyncHandler(async (req, res) => {
-  res.render('books/new', { book: {}, title: 'New Book' }); 
+  res.render('books/new-book', { book: {}, errors: false, title: 'New Book' }); 
+}));
+
+/* POST create book */
+router.post('/new', asyncHandler(async (req, res) => {
+  let book;
+  try {
+    book = await Book.create(req.body);
+    res.redirect("/books/");
+  } catch (error) {
+    if(error.name === "SequelizeValidationError") { 
+      book = await Book.build(req.body);
+      res.render("books/new-book", { errors: error.errors, title: "New Book" })
+    } else {
+      throw error; // error caught in the asyncHandler's catch block
+    }  
+  }
+}));
+
+/* GET update book form */
+router.get("/:id", asyncHandler(async(req, res) => {
+  const book = await Book.findByPk(req.params.id);
+  if(book) {
+    res.render("books/update-book", { book, title: "Update Book" });      
+  } else {
+    res.sendStatus(404);
+  }
+}));
+
+/* POST update book */
+router.post('/:id', asyncHandler(async (req, res) => {
+  let book;
+  try {
+    book = await Book.findByPk(req.params.id);
+    if(book) {
+      await book.update(req.body);
+      res.redirect("/books"); 
+    } else {
+      res.sendStatus(404);
+    }
+  } catch (error) {
+    if(error.name === "SequelizeValidationError") {
+      book = await Book.build(req.body);
+      book.id = req.params.id; // make sure correct book gets updated
+      res.render("books/update-book", { book, errors: error.errors, title: "Edit Book" })
+    } else {
+      throw error;
+    }
+  }
 }));
 
 
